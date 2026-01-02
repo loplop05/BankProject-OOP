86ca107f14e5d17f1d88e458832b6b04fa5d0228
loplop05 <ammaralharoon@hotmail.com>
2026-01-02 21:58:20 +0300

    UI Progress

---
+++ BankProject-OOP/clsShowClientsScreen.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <iostream>
+#include "clsPerson.h"
+#include "clsBankClient.h"
+#include "clsInputValidate.h"
+#include "clsUtil.h"
+#include "clsMainScreen.h"
+
+#include<iomanip>
+
+
+class clsShowClientsScreen : protected clsScreen
+{
+private :
+    void _PrintClientRecordLine(clsBankClient Client)
+    {
+
+        cout << "| " << setw(15) << left << Client.AccountNumber();
+        cout << "| " << setw(20) << left << Client.FullName();
+        cout << "| " << setw(12) << left << Client.Phone;
+        cout << "| " << setw(20) << left << Client.Email;
+        cout << "| " << setw(10) << left << Client.PinCode;
+        cout << "| " << setw(12) << left << Client.AccountBalance;
+
+    }
+
+    
+
+
+public:
+    void ShowClientsList()
+    {
+
+        vector <clsBankClient> vClients = clsBankClient::GetClientsList();
+
+        cout << "\n\t\t\t\t\tClient List (" << vClients.size() << ") Client(s).";
+        cout << "\n_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+        cout << "| " << left << setw(15) << "Accout Number";
+        cout << "| " << left << setw(20) << "Client Name";
+        cout << "| " << left << setw(12) << "Phone";
+        cout << "| " << left << setw(20) << "Email";
+        cout << "| " << left << setw(10) << "Pin Code";
+        cout << "| " << left << setw(12) << "Balance";
+        cout << "\n_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+        if (vClients.size() == 0)
+            cout << "\t\t\t\tNo Clients Available In the System!";
+        else
+
+            for (clsBankClient Client : vClients)
+            {
+
+                _PrintClientRecordLine(Client);
+                cout << endl;
+            }
+
+        cout << "\n_______________________________________________________";
+        cout << "_________________________________________\n" << endl;
+
+    }
+};
